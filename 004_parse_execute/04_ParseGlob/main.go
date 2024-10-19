d2c2c6fc8273248fae217d9892250a44c3c3ae44
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-10-19 22:37:03 +0100

    use extension .gohtml instead of .goab

--- 004_parse_execute/04_ParseGlob/main.go
+++ 004_parse_execute/04_ParseGlob/main.go
@@ -17,17 +17,17 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	err = tpl.ExecuteTemplate(os.Stdout, "two.goab", nil)
+	err = tpl.ExecuteTemplate(os.Stdout, "two.gohtml", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	err = tpl.ExecuteTemplate(os.Stdout, "vespa.goab", nil)
+	err = tpl.ExecuteTemplate(os.Stdout, "vespa.gohtml", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = tpl.ExecuteTemplate(os.Stdout, "one.goab", nil)
+	err = tpl.ExecuteTemplate(os.Stdout, "one.gohtml", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
